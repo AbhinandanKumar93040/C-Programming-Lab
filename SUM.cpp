bc53d9b279b94b66919f3e4338ac825c6795e668
AbhinandanKumar93040 <meb25054@tezu.ac.in>
2025-10-14 17:07:44 +0530

    Add files via upload

---
+++ SUM.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    printf("enter a:");
+    scanf ("%d",&a);
+    printf("entre b:");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("%d" , sum);
+    return 0;
+}
