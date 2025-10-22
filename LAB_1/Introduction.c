0bd23308c2311388e172adc758840c7fba27ce9e
AbhinandanKumar93040 <meb25054@tezu.ac.in>
2025-10-22 15:21:50 +0530

    Add Introduction.c with student details

---
+++ LAB_1/Introduction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name : Abhinandan Kumar\n");
+    printf("Course : Bachelor of Technology\n");
+    printf("Branch : Mechanical Engineering\n");
+    printf("Roll No. : MEB25054\n");    
+    printf("College : Tezpur University");
+    return 0;
+}
